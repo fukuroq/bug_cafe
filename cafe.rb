f8a6296659b7edd65e656d5f6344f349120542d7
fukuroq <fukurou.bbb.23@gmail.com>
2024-05-08 11:14:54 +0900

    注文された飲み物と注文された食べ物の変数名をそれぞれordered_drinkとordered_foodに変更

--- cafe.rb
+++ cafe.rb
@@ -25,10 +25,10 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+ordered_drink = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+ordered_food = take_order(FOODS)
 
-total = DRINKS[order1][:price] + FOODS[order2][:price]
+total = DRINKS[ordered_drink][:price] + FOODS[ordered_food][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
